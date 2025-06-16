b5da35665955cff3daf384643c962f48386ae9a4
Nate <nate@qstreet.org>
2025-06-16 15:51:40 -0400

    Craete thing component

--- src/app/tabs.tsx
+++ src/app/tabs.tsx
@@ -14,7 +14,7 @@ export default function Tabs() {
 
   return (
     <>
-      <div className="flex rounded-md bg-shade p-1">
+      <div className="mb-2 flex rounded-md bg-shade p-1">
         {tabs.map(({ label }, index) => (
           <button
             onClick={() => setTab(index)}
@@ -26,7 +26,7 @@ export default function Tabs() {
         ))}
       </div>
 
-      <div className="mt-2">{tabs[tab].component}</div>
+      <div>{tabs[tab].component}</div>
     </>
   )
 }
@@ -34,29 +34,17 @@ export default function Tabs() {
 function Projects() {
   return (
     <div className="flex flex-col gap-2">
-      <div className="trap">
-        <h2 className="text-2xl font-bold">Oderum</h2>
-        <p className="mb-2 italic">Next, Tailwind, Drizzle, Postgres, Stripe</p>
+      <Thing
+        title="Oderum"
+        subtitle="Next, Tailwind, Drizzle, Postgres, Stripe"
+        description="Oderum is an all in one fragrance collection website. Use AI to discover new fragrances, search through notes, and write reviews. Create lists of your favorite fragrances and customize your profile. Tools allow users to filter fragrances, build pyramids, and more."
+      />
 
-        <p className="text-muted">
-          Oderum is an all in one fragrance collection website. Use AI to
-          discover new fragrances, search through notes, and write reviews.
-          Create lists of your favorite fragrances and customize your profile.
-          Tools allow users to filter fragrances, build pyramids, and more.
-        </p>
-      </div>
-
-      <div className="trap">
-        <h2 className="text-2xl font-bold">Jazzio</h2>
-        <p className="mb-2 italic">React, Tailwind, Firebase</p>
-
-        <p className="text-muted">
-          Jazzio is a membership, verification, and auditing platform for the US
-          Embassy Addis Ababa. It leverages QR codes to streamline admissions
-          and implements analytics for event holders. Designed to support both
-          English and Amharic speakers, Jazzio has gained over 20,000 members.
-        </p>
-      </div>
+      <Thing
+        title="Jazzio"
+        subtitle="React, Tailwind, Firebase"
+        description="Jazzio is a membership, verification, and auditing platform for the US Embassy Addis Ababa. It leverages QR codes to streamline admissions and implements analytics for event holders. Designed to support both English and Amharic speakers, Jazzio has gained over 20,000 members."
+      />
     </div>
   )
 }
@@ -64,27 +52,17 @@ function Projects() {
 function Experience() {
   return (
     <div className="flex flex-col gap-2">
-      <div className="trap">
-        <h2 className="text-2xl font-bold">Software Engineer Intern</h2>
-        <p className="mb-2 italic">
-          US Embassy Addis Ababa, May 2024 - Jun 2024
-        </p>
+      <Thing
+        title="Software Engineer Intern"
+        subtitle="US Embassy Addis Ababa, May 2024 - Jun 2024"
+        description="Designed an inventory management platform, implemented autonomous paperwork systems, and aided with technical support."
+      />
 
-        <p className="text-muted">
-          Designed an inventory management platform, implemented autonomous
-          paperwork systems, and aided with technical support.
-        </p>
-      </div>
-
-      <div className="trap">
-        <h2 className="text-2xl font-bold">Freelance Web Developer</h2>
-        <p className="mb-2 italic">Various Locations, Aug 2023 - May 2025</p>
-
-        <p className="text-muted">
-          Built websites for several non-profit organizations based in Africa,
-          implementing donation platforms and membership management tools.
-        </p>
-      </div>
+      <Thing
+        title="Freelance Web Developer"
+        subtitle="Various Locations, Aug 2023 - May 2025"
+        description="Built websites for several non-profit organizations based in Africa, implementing donation platforms and membership management tools."
+      />
     </div>
   )
 }
@@ -92,28 +70,36 @@ function Experience() {
 function Education() {
   return (
     <div className="flex flex-col gap-2">
+      <Thing
+        title="Bachelor of Science in Computer Science"
+        subtitle="University of Pittsburgh, Aug 2025 - May 2029"
+        description="Details coming soon"
+      />
+
+      <Thing
+        title="International Baccalaureate"
+        subtitle="International Community School of Addis Ababa, Aug 2021 - May 2024"
+        description="Details coming soon"
+      />
+    </div>
+  )
+}
+
+function Thing({
+  title,
+  subtitle,
+  description,
+}: {
+  title: string
+  subtitle: string
+  description: string
+}) {
+  return (
     <div className="trap">
-      <h2 className="text-2xl font-bold">
-        Bachelor of Science in Computer Science
-      </h2>
-      <p className="mb-2 italic">
-        University of Pittsburgh, Aug 2025 - May 2029
-      </p>
-
-      <p className="text-muted">Details coming soon</p>
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <p className="mb-2 italic">{subtitle}</p>
+      <p className="text-muted">{description}</p>
     </div>
-
-      <div className="trap">
-      <h2 className="text-2xl font-bold">
-International Baccalaureate      
-      </h2>
-      <p className="mb-2 italic">
-        International Community School of Addis Ababa, Aug 2021 - May 2024
-      </p>
-
-      <p className="text-muted">Details coming soon</p>
-    </div>
-      </div>
   )
 }
 
